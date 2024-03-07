ffcd8fe92b351b53a2d315364b496605f1f0efa5
Pascal Welke <pascal.welke@tuwien.ac.at>
2024-03-07 10:06:31 +0100

    fix url handling of papers

--- generate_bib.py
+++ generate_bib.py
@@ -23,10 +23,12 @@ def cf(s1, s2, format):
 
 def format_bibitem(item, format):
     '''the main workhorse'''
-    if 'doi' in item:
+    if nonempty('doi', item):
         link = f'https://dx.doi.org/{item["doi"]}'
-    elif 'url' in item:
+    elif nonempty('url', item):
         link = item['url']
+    elif nonempty('pdf', item):
+        link = item['pdf']
     else:
         link = ''
 
@@ -125,7 +127,7 @@ def authorfirstlast(record):
 
 def customizations(record):
     '''A function to customize our entries.
-    It takes a record and return this record.'''
+    It takes a record and returns this record.'''
 
     record = convert_to_unicode(record)
     record = authorfirstlast(record)
